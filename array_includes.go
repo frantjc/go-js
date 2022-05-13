a5df3e146d7706eadecc8debab63cb2d7b442c77
Frantjc <logsdonalex11@gmail.com>
2022-05-12 22:29:43 -0400

    refactor a bit to be more DRY

--- array_includes.go
+++ array_includes.go
@@ -3,10 +3,7 @@ package js
 // Includes determines whether an array includes a certain value
 // among its entries, returning true or false as appropriate.
 func Includes[T1 comparable](in []T1, a T1) bool {
-	for _, b := range in {
-		if a == b {
-			return true
-		}
-	}
-	return false
+	return Some(in, func(b T1, _ int, _ []T1) bool {
+		return a == b
+	})
 }
--- array_index_of.go
+++ array_index_of.go
@@ -3,10 +3,7 @@ package js
 // IndexOf returns the first index at which a given element
 // can be found in the array, or -1 if it is not present.
 func IndexOf[T1 comparable](in []T1, a T1) int {
-	for i, b := range in {
-		if a == b {
-			return i
-		}
-	}
-	return -1
+	return FindIndex(in, func(b T1, _ int, _ []T1) bool {
+		return a == b
+	})
 }
